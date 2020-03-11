7f5cc84b63898a6db23f04de5323b1998b3c9b98
Lennard Grimm <52577845+grimmle@users.noreply.github.com>
2020-03-11 22:36:14 +0100

    Update workplace_list_component.js

--- frontend/src/workplace_list_component.js
+++ frontend/src/workplace_list_component.js
@@ -4,8 +4,8 @@ import moment from 'moment';
 
 import './style.css';
 
-//const URL = "https://office-occupancy.herokuapp.com/workplaces"
-const URL = "http://localhost:5000/workplaces"
+const URL = "https://office-occupancy.herokuapp.com/workplaces"
+//const URL = "http://localhost:5000/workplaces"
 
 export default class WorkplaceListComponent extends React.Component {
   constructor(props) {
